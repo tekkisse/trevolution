a179e4ed1ec9f26e0e699a9bba634824a51fad33
Simon Thompson <simon@chi.swan.ac.uk>
2025-03-10 13:51:58 +0000

    tidy up

--- windows/config.bat
+++
@@ -1,9 +0,0 @@
-echo "Installing MicroK8s Plugins"
-echo "- DNS"
-microk8s enable dns
-echo "- Dashboard"
-microk8s enable dashboard
-echo "- Helm"
-microk8s enable helm
-echo "- Storage"
-microk8s enable hostpath-storage
